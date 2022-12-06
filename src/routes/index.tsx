645d2e3709cf52c6b3ed5f2c7645242aa62ea295
torvic <torvic02st96@gmail.com>
2022-12-06 16:58:41 -0500

    feat: add react native navigation & react native vector icons

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import BottomTabNav from './ButtomTabNav';
+
+const Root = createStackNavigator();
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Root.Navigator screenOptions={{headerShown: false}}>
+        <Root.Screen name="HomeTabs" component={BottomTabNav} />
+      </Root.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
